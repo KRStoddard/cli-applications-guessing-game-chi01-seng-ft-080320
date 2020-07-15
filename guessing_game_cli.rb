f8fbbe414d6b03ee83854e0b88bab49f0e2cb19e
Kimberlyn Stoddard <kimberlynrstoddard@gmail.com>
2020-07-15 19:28:06 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -16,8 +16,30 @@ def end_game
   puts "Goodbye!"
 end 
 
+def are_you_right(number)
+  prompt_user 
+  user_guess = get_user_input
+  if user_guess == "#{number}"
+    puts "You guessed the correct number!"
+    elsif user_guess == "exit"
+    end_game 
+  else 
+    puts "Sorry! The computer guessed #{number}."
+  end
+end
 
-
+def run_guessing_game
+random_number = generate_number
+prompt_user 
+  user_guess = get_user_input
+  if user_guess == "#{number}"
+    puts "You guessed the correct number!"
+    elsif user_guess == "exit"
+    end_game 
+  else 
+    puts "Sorry! The computer guessed #{number}."
+  end
+end
 def run_guessing_game
   rando = rand(6) + 1 
   puts "Guess a number between 1 and 6:"
